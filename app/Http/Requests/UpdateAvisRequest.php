fc92b0583164c381068b3ec585e91923d44281d4
celinemend <celine.mendy@unchk.edu.sn>
2024-09-30 11:35:41 +0000

    tout les fonctionnalité bien mis en place tout fonction correctement

--- app/Http/Requests/UpdateAvisRequest.php
+++ app/Http/Requests/UpdateAvisRequest.php
@@ -11,7 +11,7 @@ class UpdateAvisRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,8 @@ class UpdateAvisRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'commentaire' => 'sometimes|required|string|max:255',
+            'note' => 'sometimes|required|integer|min:1|max:5',
         ];
     }
 }
